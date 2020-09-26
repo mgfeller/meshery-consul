91f82d23fb53e6b69ba5e8e8c55611bed39c6b20
Michael Gfeller <mgfeller@mgfeller.net>
2020-09-26 12:05:09 +0200

    added comments to trigger build on github
    Signed-off-by: Michael Gfeller <mgfeller@mgfeller.net>

--- common/helm/helm.go
+++ common/helm/helm.go
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//
 // Based on https://github.com/PrasadG193/helm-clientgo-example.git
+//
 
 package helm
 
